a0cb5c1da958274706f9f34dc5b64d9ca18b8374
shahida314 <143307882+shahida314@users.noreply.github.com>
2026-06-09 16:09:56 +0600

    Update Problem2.py

--- Task 2/Problem2.py
+++ Task 2/Problem2.py
@@ -1,1 +1,20 @@
 
+t = input().strip()
+k = int(input())
+fq = {}
+for i in range(len(t)-k+1):
+    kmer=t[i:i+k]
+    fq[kmer] = fq.get(kmer,0) + 1
+    
+    
+m_c = max(fq.values())
+     
+          
+seen = set()
+for i in range(len(t)-k+1):
+                    kmer = t[i:i+k]
+                    if fq[kmer] == m_c and kmer not in seen:
+                
+                        print(kmer, end=" ")
+                        seen.add(kmer)
+            
