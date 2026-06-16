9373e4f50b8fc84df0b6cbb5c1159ce128fb528b
shahida314 <143307882+shahida314@users.noreply.github.com>
2026-06-16 16:15:19 +0600

    Update Problem1.py

--- Task 3/Problem1.py
+++ Task 3/Problem1.py
@@ -1,1 +1,42 @@
+t = input().strip()
+k, d = map(int, input().split())
 
+def hamming(a, b):
+    c = 0
+    for i in range(len(a)):
+        if a[i] != b[i]:
+            c += 1
+    return c
+
+patterns = []
+
+def generate(crnt):
+    if len(crnt) == k:
+        patterns.append(crnt)
+        return
+
+    generate(crnt + "A")
+    generate(crnt + "C")
+    generate(crnt + "G")
+    generate(crnt + "T")
+
+generate("")
+
+fq = {}
+
+for pattern in patterns:
+    c = 0
+
+    for i in range(len(t) - k + 1):
+        kmer = t[i:i+k]
+
+        if hamming(pattern, kmer) <= d:
+            c += 1
+
+    fq[pattern] = c
+
+max_c = max(fq.values())
+
+for pattern in fq:
+    if fq[pattern] == max_c:
+        print(pattern, end=" ")
